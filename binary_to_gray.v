50d62cd778289593ec8beb82c7afd6d87aae83d4
Zazamartins <isaacmrtnsss@gmail.com>
2025-04-23 10:23:56 -0300

    Adicionando tarefa

--- binary_to_gray.v
+++ binary_to_gray.v
@@ -3,6 +3,12 @@ module Binary_to_Gray (
     output wire [3:0] gray_out
 );
 
-// Insira seu codigo aqui
+    // O bit mais significativo (MSB) permanece o mesmo
+    assign gray_out[3] = binary_in[3];
+    
+    // Cada bit seguinte é o XOR do bit binário atual com o próximo mais significativo
+    assign gray_out[2] = binary_in[3] ^ binary_in[2];
+    assign gray_out[1] = binary_in[2] ^ binary_in[1];
+    assign gray_out[0] = binary_in[1] ^ binary_in[0];
 
 endmodule
--- gray_to_binary.v
+++ gray_to_binary.v
@@ -3,6 +3,12 @@ module Gray_to_Binary (
     output wire [3:0] binary_out
 );
 
-// Insira seu codigo aqui
+    // O bit mais significativo (MSB) permanece o mesmo
+    assign binary_out[3] = gray_in[3];
+    
+    // Cada bit seguinte é o XOR do bit binário anterior com o bit gray atual
+    assign binary_out[2] = binary_out[3] ^ gray_in[2];
+    assign binary_out[1] = binary_out[2] ^ gray_in[1];
+    assign binary_out[0] = binary_out[1] ^ gray_in[0];
 
 endmodule
